a4879a5769d5aefd3b49e8c33540cea2f0e42c20
Med KTITE <ktite.m3@gmail.com>
2024-11-18 20:56:11 +0100

    ✨ Add configuration gateway-service

---
+++ gateway-serviice/src/test/java/invenntory/gatewayserviice/GatewayServiiceApplicationTests.java
@@ -0,0 +1,13 @@
+package invenntory.gatewayserviice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
